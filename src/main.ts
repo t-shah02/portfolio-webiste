139fba1c9480d293f1f52477a37d59b602d25a82
Tanish Shah <connecttanish@gmail.com>
2023-08-31 22:37:45 -0600

    added typewriter effect and resume link

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import "./style.css";
+// @ts-ignore
+import Typed from 'typed.js';
 
+const consoleCommandsAndOutputs = [
+    "cat hello_world.py",
+    "print('Hello World')",
+    "cat helloWorld.ts",
+    "console.log('Hello World');",
+    "cat HelloWorld.java",
+    'System.out.println("Hello World");',
+    "cat helloworld.go",
+    'fmt.Println("Hello World") '
+];
 
-const footerYearParagraph: HTMLParagraphElement | null = document.querySelector("#footer-year-p");
-
-if (footerYearParagraph) {
-    const currentYear = new Date(Date.now()).getFullYear();
-    footerYearParagraph.innerText = `© Tanish Shah. ${currentYear}. All rights reserved.`
-}
+new Typed('#prog-lang-heading', {
+    strings: consoleCommandsAndOutputs,
+    typeSpeed: 50,
+    backSpeed: 50,
+    loop: true,
+    smartBackspace: false,
+});
